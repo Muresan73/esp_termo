6492661e30e2f67ee3ee264462a9c3b457accf4b
Gergo <gergely@storekey.com>
2023-09-05 15:19:40 +0000

    working simple MQTT client

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,36 @@
 use anyhow::Result;
 use embedded_hal::delay::DelayUs;
-use embedded_svc::mqtt::client::QoS;
-use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::{delay::FreeRtos, prelude::Peripherals};
 
 // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use esp_idf_sys as _;
 
 mod mqtt;
 mod sensor;
-use crate::{mqtt::new_mqqt_client, sensor::new_bme280};
+mod wifi;
+use crate::{
+    mqtt::{new_mqqt_client, SimpleMqttClient},
+    sensor::new_bme280,
+};
 
 fn main() -> Result<()> {
-    esp_idf_sys::link_patches();
     println!("program started :)");
+    esp_idf_sys::link_patches();
+    esp_idf_svc::log::EspLogger::initialize_default();
 
-    // let mut bme280 = new_bme280()?;
+    let peripherals = Peripherals::take().unwrap();
+
+    let _wifi = wifi::connect(peripherals.modem)?;
+
     let mut mqqt = new_mqqt_client()?;
+    // let mut bme280 = new_bme280(peripherals)?;
+    // http_server::start_server()?;
 
     loop {
         // 5. This loop initiates measurements, reads values and prints humidity in % and Temperature in °C.
         FreeRtos.delay_ms(100u32);
 
-        mqqt.publish(
-            format!("{}/feeds/temperature", "USERNAME").as_str(),
-            QoS::AtMostOnce,
-            false,
-            format!("{}", 11).as_bytes(),
-        )?;
-        println!("Published message");
+        mqqt.message("test".into())?;
 
         // if let Some(humidity) = bme280.read_humidity()? {
         //     println!("Humidity: {:.2} %", humidity);
@@ -44,7 +47,7 @@ fn main() -> Result<()> {
         // } else {
         //     println!("Pressure reading was disabled");
         // }
-        println!();
+        println!("Waiting 5 seconds");
         FreeRtos.delay_ms(5000u32);
     }
 }
