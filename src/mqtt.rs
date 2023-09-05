90b5c6c1c4efce775b9c2bd66d6053e1b608ffb2
Gergo <grge73@outlook.com>
2023-09-05 15:19:40 +0000

    working simple MQTT client

--- src/mqtt.rs
+++ src/mqtt.rs
@@ -1,73 +1,28 @@
-use std::sync::Arc;
-use std::thread;
-use std::thread::sleep;
+use anyhow::Result;
+use core::str;
+
 use std::time::Duration;
 
-use anyhow::bail;
 use dotenvy_macro::dotenv;
-use embedded_svc::wifi::AuthMethod;
-use embedded_svc::wifi::ClientConfiguration;
 
-use embedded_svc::wifi::Configuration;
-use embedded_svc::wifi::Wifi;
-use esp_idf_hal::i2c;
-use esp_idf_hal::peripherals::Peripherals;
-use esp_idf_hal::prelude::*;
-use esp_idf_svc::eventloop::EspSystemEventLoop;
 use esp_idf_svc::mqtt::client::LwtConfiguration;
-use esp_idf_svc::wifi;
+use esp_idf_svc::mqtt::client::MqttProtocolVersion;
 // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use esp_idf_sys as _;
 
-use anyhow::Result;
-use embedded_svc::mqtt::client::{Connection, Publish, QoS};
+use embedded_svc::mqtt::client::QoS;
 use esp_idf_svc::mqtt::client::{EspMqttClient, MqttClientConfiguration};
-use esp_idf_svc::netif::*;
-use esp_idf_svc::nvs::*;
-use esp_idf_svc::wifi::*;
 
-const SSID: &str = dotenv!("SSID");
-const PASSWORD: &str = dotenv!("PASSWORD");
 const USERNAME: &str = dotenv!("USERNAME");
 const KEY: &str = dotenv!("KEY");
-
-pub fn connect_2_wifi() -> Result<()> {
-    let peripherals = Peripherals::take().unwrap();
-    let sys_loop = EspSystemEventLoop::take()?;
-    let nvs = EspDefaultNvsPartition::take()?;
-
-    let mut wifi = BlockingWifi::wrap(
-        EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?,
-        sys_loop,
-    )?;
-    let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
-        ssid: SSID.into(),
-        bssid: None,
-        auth_method: AuthMethod::WPA2Personal,
-        password: PASSWORD.into(),
-        channel: None,
-    });
-
-    wifi.set_configuration(&wifi_configuration)?;
-
-    wifi.start()?;
-    println!("Wifi started");
-
-    wifi.connect()?;
-    println!("Wifi connected");
-
-    wifi.wait_netif_up()?;
-    println!("Wifi netif up");
-
-    Ok(())
-}
+const MQTT_SERVER: &str = dotenv!("MQTT_SERVER");
 
 pub fn new_mqqt_client() -> Result<EspMqttClient> {
-    connect_2_wifi()?;
-
-    let error_topic = format!("{}/error", USERNAME);
+    let error_topic = format!("error/{}", USERNAME);
     let conf = MqttClientConfiguration {
         client_id: Some("esp32-sensore"),
+        protocol_version: Some(MqttProtocolVersion::V3_1_1),
+
         keep_alive_interval: Some(Duration::from_secs(120)),
         lwt: Some(LwtConfiguration {
             topic: error_topic.as_str(),
@@ -79,8 +34,8 @@ pub fn new_mqqt_client() -> Result<EspMqttClient> {
     };
 
     println!("MQTT Conecting ...");
-    let client = EspMqttClient::new(
-        format!("mqtt://{}:{}@test.mosquitto.org3", USERNAME, KEY),
+    let mut client = EspMqttClient::new(
+        format!("mqtt://{MQTT_SERVER}"),
         &conf,
         move |msg| match msg {
             Ok(msg) => println!("MQTT Message: {:?}", msg),
@@ -89,6 +44,12 @@ pub fn new_mqqt_client() -> Result<EspMqttClient> {
     )?;
     println!("MQTT Listening for messages");
 
+    client.publish(
+        "status/sensor",
+        QoS::AtMostOnce,
+        false,
+        "connected".to_string().as_bytes(),
+    )?;
     // loop {
     //     println!("Before publish");
 
@@ -108,37 +69,13 @@ pub fn new_mqqt_client() -> Result<EspMqttClient> {
     Ok(client)
 }
 
-// fn wifi(
-//     netif_stack: Arc<EspNetifStack>,
-//     sys_loop_stack: Arc<EspSysLoopStack>,
-//     default_nvs: Arc<EspDefaultNvs>,
-// ) -> Result<Box<EspWifi>> {
-//     let mut wifi = Box::new(EspWifi::new(netif_stack, sys_loop_stack, default_nvs)?);
+pub trait SimpleMqttClient {
+    fn message(&mut self, msg: String) -> Result<()>;
+}
 
-//     wifi.set_configuration(&Configuration::Client(ClientConfiguration {
-//         ssid: SSID.into(),
-//         password: PASS.into(),
-//         ..Default::default()
-//     }))?;
-
-//     println!("Wifi configuration set, about to get status");
-
-//     wifi.wait_status_with_timeout(Duration::from_secs(20), |status| !status.is_transitional())
-//         .map_err(|e| anyhow::anyhow!("Unexpected Wifi status: {:?}", e))?;
-
-//     let status = wifi.get_status();
-
-//     if let Status(
-//         ClientStatus::Started(ClientConnectionStatus::Connected(ClientIpStatus::Done(
-//             _ip_settings,
-//         ))),
-//         _,
-//     ) = status
-//     {
-//         println!("Wifi connected");
-//     } else {
-//         bail!("Unexpected Wifi status: {:?}", status);
-//     }
-
-//     Ok(wifi)
-// }
+impl SimpleMqttClient for EspMqttClient {
+    fn message(&mut self, msg: String) -> Result<()> {
+        self.publish("feeds/message", QoS::AtMostOnce, false, msg.as_bytes())?;
+        Ok(())
+    }
+}
---
+++ src/wifi.rs
@@ -0,0 +1,52 @@
+use anyhow::Result;
+use core::str;
+use embedded_svc::wifi::Configuration;
+use esp_idf_svc::eventloop::EspSystemEventLoop;
+
+use dotenvy_macro::dotenv;
+use embedded_svc::wifi::AuthMethod;
+use embedded_svc::wifi::ClientConfiguration;
+
+use esp_idf_sys as _;
+
+use esp_idf_svc::nvs::*;
+use esp_idf_svc::wifi::*;
+
+const SSID: &str = dotenv!("SSID");
+const PASSWORD: &str = dotenv!("PASSWORD");
+
+pub fn connect(modem: esp_idf_hal::modem::Modem) -> Result<Box<EspWifi<'static>>> {
+    println!("Wifi getting resources");
+
+    let sys_loop = EspSystemEventLoop::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
+
+    println!("Wifi aquired resources");
+    let mut esp_wifi = EspWifi::new(modem, sys_loop.clone(), Some(nvs))?;
+    let mut wifi = BlockingWifi::wrap(&mut esp_wifi, sys_loop)?;
+
+    println!("Wifi blocker");
+
+    let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
+        ssid: SSID.into(),
+        bssid: None,
+        auth_method: AuthMethod::WPA2Personal,
+        password: PASSWORD.into(),
+        channel: None,
+    });
+
+    println!("Wifi configured");
+
+    wifi.set_configuration(&wifi_configuration)?;
+
+    wifi.start()?;
+    println!("Wifi started");
+
+    wifi.connect()?;
+    println!("Wifi connected");
+
+    wifi.wait_netif_up()?;
+    println!("Wifi netif up");
+
+    Ok(Box::new(esp_wifi))
+}
