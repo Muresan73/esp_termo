589bea4b2b46e29c0118cfb48bde6bdbb33d39ab
Gergo <grge73@outlook.com>
2023-09-11 12:22:07 +0000

    unified measurement messaging

--- src/sensor.rs
+++ src/sensor.rs
@@ -15,8 +15,40 @@ use esp_idf_sys::EspError;
 use log::{error, info};
 use serde_json::json;
 
-pub trait MessageAble<T, E> {
-    fn to_json(&mut self) -> Option<String>;
+pub trait MessageAble<T>
+where
+    T: std::fmt::Display,
+{
+    fn to_json(&mut self) -> Option<String> {
+        {
+            let json_vec: Option<Vec<_>> = self
+                .get_measurment_vec()
+                .iter()
+                .map(|(m_type, unit, measurement, status)| {
+                    if let (Some(msrmnt), Some(stts)) = (measurement, status) {
+                        Some(json!( {
+                                "type":m_type,
+                                "value": msrmnt,
+                                "status": stts.to_string(),
+                                "unit": unit
+                        }))
+                    } else {
+                        log::warn!("Error reading {m_type}");
+                        None
+                    }
+                })
+                .collect();
+
+            if let Some(json_vec) = json_vec {
+                Some(json!({ "measurements": json_vec }).to_string())
+            } else {
+                error!("Sensors are not connected or readings are invalid");
+                None
+            }
+        }
+    }
+
+    fn get_measurment_vec(&mut self) -> Vec<(&str, &str, Option<f32>, Option<T>)>;
 }
 
 // ================
@@ -65,35 +97,64 @@ pub fn new_bme280<I2C: I2c>(
     Ok(bme280)
 }
 
-impl MessageAble<String, Bme280Error> for Bme280<I2cDriver<'static>, Delay> {
-    fn to_json(&mut self) -> Option<String> {
-        let json_vec: Option<Vec<_>> = [
-            ("pressure", "Pa", self.read_pressure()),
-            ("temperature", "°C", self.read_temperature()),
-            ("humidity", "%", self.read_humidity()),
-        ]
-        .map(|(m_type, unit, measurement)| {
-            if let Ok(Some(msrmnt)) = measurement {
-                Some(json!( {
-                        "type":m_type,
-                        "value": format!("{:.2}",msrmnt),
-                        "unit": unit
+pub trait Bme280Extention {
+    fn read_temperature_status(&mut self) -> Option<String>;
+    fn read_pressure_status(&mut self) -> Option<String>;
+    fn read_humidity_status(&mut self) -> Option<String>;
+}
 
-                }))
-            } else {
-                log::warn!("Error reading {m_type}");
-                None
-            }
-        })
-        .into_iter()
-        .collect();
-
-        if let Some(json_vec) = json_vec {
-            Some(json!({ "measurements": json_vec }).to_string())
-        } else {
-            error!("Sensors are not connected or readings are invalid");
-            None
+impl Bme280Extention for Bme280<I2cDriver<'static>, Delay> {
+    fn read_temperature_status(&mut self) -> Option<String> {
+        let temp = self.read_temperature().ok()??;
+        match temp {
+            t if t < 0.0 => Some("Freezing".to_string()),
+            t if t < 18.0 => Some("Cold".to_string()),
+            t if t < 25.0 => Some("Optimal".to_string()),
+            _ => Some("Hot".to_string()),
         }
+    }
+    fn read_humidity_status(&mut self) -> Option<String> {
+        let humidity = self.read_humidity().ok()??;
+        match humidity {
+            h if h < 30.0 => Some("Dry".to_string()),
+            h if h < 50.0 => Some("Optimal".to_string()),
+            h if h < 70.0 => Some("Moist".to_string()),
+            _ => Some("Wet".to_string()),
+        }
+    }
+    fn read_pressure_status(&mut self) -> Option<String> {
+        let pressure = self.read_pressure().ok()??;
+        match pressure {
+            p if p < 1000.0 => Some("Low".to_string()),
+            p if p < 1013.0 => Some("Optimal".to_string()),
+            _ => Some("High".to_string()),
+        }
+    }
+}
+
+impl MessageAble<String> for Bme280<I2cDriver<'static>, Delay> {
+    fn get_measurment_vec(&mut self) -> Vec<(&str, &str, Option<f32>, Option<String>)> {
+        [
+            (
+                "temperature",
+                "°C",
+                self.read_temperature().unwrap_or(None),
+                self.read_temperature_status(),
+            ),
+            (
+                "humidity",
+                "%",
+                self.read_humidity().unwrap_or(None),
+                self.read_humidity_status(),
+            ),
+            (
+                "pressure",
+                "hPa",
+                self.read_pressure().unwrap_or(None),
+                self.read_pressure_status(),
+            ),
+        ]
+        .to_vec()
     }
 }
 
@@ -108,6 +169,7 @@ const MOISTURE_RANGE: u16 = MAX_DRY - MAX_WET;
 const FULL_PRECENTAGE: f32 = 100.0;
 const NO_PRECENTAGE: f32 = 0.0;
 
+#[derive(Clone)]
 pub enum SoilStatus {
     Dry,
     Optimal,
@@ -172,52 +234,29 @@ where
     /// Optimal -> 20-40%
     /// Da -> 40-55%
     /// Wet -> 55-100%
-    pub fn get_soil_status(&mut self) -> MoistureResult<SoilStatus> {
-        let percentage = self.get_moisture_precentage()?;
+    pub fn get_soil_status(&mut self) -> Option<SoilStatus> {
+        let percentage = self.get_moisture_precentage().ok()?;
 
         match percentage {
-            p if p < 20.0 => Ok(SoilStatus::Dry),
-            p if p < 40.0 => Ok(SoilStatus::Optimal),
-            p if p < 55.0 => Ok(SoilStatus::Damp),
-            _ => Ok(SoilStatus::Wet),
+            p if p < 20.0 => Some(SoilStatus::Dry),
+            p if p < 40.0 => Some(SoilStatus::Optimal),
+            p if p < 55.0 => Some(SoilStatus::Damp),
+            _ => Some(SoilStatus::Wet),
         }
     }
 }
 
-impl<T> MessageAble<String, EspError> for SoilMoisture<'_, T>
+impl<T> MessageAble<SoilStatus> for SoilMoisture<'_, T>
 where
     T: ADCPin<Adc = ADC1>,
 {
-    fn to_json(&mut self) -> Option<String> {
-        {
-            let json_vec: Option<Vec<_>> = [(
-                "soil-moisture",
-                "%",
-                self.get_moisture_precentage(),
-                self.get_soil_status(),
-            )]
-            .map(|(m_type, unit, measurement, status)| {
-                if let (Ok(msrmnt), Ok(stts)) = (measurement, status) {
-                    Some(json!( {
-                            "type":m_type,
-                            "value": msrmnt,
-                            "status": stts.to_string(),
-                            "unit": unit
-                    }))
-                } else {
-                    log::warn!("Error reading {m_type}");
-                    None
-                }
-            })
-            .into_iter()
-            .collect();
-
-            if let Some(json_vec) = json_vec {
-                Some(json!({ "measurements": json_vec }).to_string())
-            } else {
-                error!("Sensors are not connected or readings are invalid");
-                None
-            }
-        }
+    fn get_measurment_vec(&mut self) -> Vec<(&str, &str, Option<f32>, Option<SoilStatus>)> {
+        [(
+            "soil-moisture",
+            "%",
+            self.get_moisture_precentage().ok(),
+            self.get_soil_status(),
+        )]
+        .to_vec()
     }
 }
